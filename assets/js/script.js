d704a08d5ea66055849bdd6b7c2f2ea6810bc8d6
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-05-15 15:49:10 +0200

    Add Twitter logo image to assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
-console.log('Weee! I am a JavaScript file!');
+// Carusel functionality
+
+// Form validation
+
+document.getElementById('validate').addEventListener('click', function () {
+  const fullname = this.form.fullname;
+  const email = this.form.email;
+
+  if (!fullname.value) {
+    displayError(fullname, 'Du skal udfyde dette felt');
+    return;
+  }
+  if (!email.value) {
+    displayError(email, 'Du skal udfyde dette felt');
+  }
+});
+
+const displayError = (element, message) => {
+  const parent = element.parentElement;
+  const grandparent = parent.parentElement;
+
+  // Remove any existing error message
+  if (grandparent.querySelector('.error-message')) {
+    grandparent.querySelector('.error-message').remove();
+  }
+
+  // Add error class and message
+  grandparent.classList.add('error');
+  const errorMessage = document.createElement('span');
+  errorMessage.className = 'error-message';
+  errorMessage.textContent = message;
+
+  parent.appendChild(errorMessage);
+};
+
+document.querySelectorAll('input').forEach((element) => {
+  element.addEventListener('input', function () {
+    const parent = this.parentElement;
+    const grandparent = parent.parentElement;
+
+    // Remove error message and class on input
+    if (grandparent.querySelector('.error-message')) {
+      grandparent.querySelector('.error-message').remove();
+    }
+    grandparent.classList.remove('error');
+  });
+});
